dcc47e0974f761d8d365ad2e232e239d08dc5f4c
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-02-08 14:29:35 +0330

    add navigations

--- Appetizers/AccountView.swift
+++ Appetizers/AccountView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            Text("Account view")
+                .navigationTitle("👤 Account")
+        }
+    
     }
 }
 
