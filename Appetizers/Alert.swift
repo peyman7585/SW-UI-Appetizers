101995445016bce12b58e9c9e56517fc17ebdf67
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-02-11 18:42:45 +0330

    add alert and Error handler

---
+++ Appetizers/Alert.swift
@@ -0,0 +1,31 @@
+//
+//  Alert.swift
+//  Appetizers
+//
+//  Created by Peyman on 2/11/25.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+    
+    struct AlertContext {
+        static let invalidData      = AlertItem(title: Text("Server Error"),
+                                                message: Text("The data recevied from the serve was invalid. Please contact support"),
+                                                dismissButton: .default(Text("OK")))
+        static let invalidResponse       = AlertItem(title: Text("Server Error"),
+                                                message: Text("Invalid response from the server. please try again later or contact to support"),
+                                                dismissButton: .default(Text("OK")))
+        static let invalidURL  = AlertItem(title: Text("Server Error"),
+                                                message: Text("There was an issue connecting to the server. If this persists. Please contact support"),
+                                                dismissButton: .default(Text("OK")))
+        static let unableToComplete = AlertItem(title: Text("Server Error"),
+                                                message: Text("Unable to complete your request at this time. Please check your internet connection"),
+                                                dismissButton: .default(Text("OK")))
+    
+}
---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Peyman on 2/11/25.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { [self] result in
+            DispatchQueue.main.sync {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    switch error{
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                        
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
+    }
+}
