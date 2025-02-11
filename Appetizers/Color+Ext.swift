e359e82e0823197bd9fd868dae45707111458f11
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-02-11 19:08:54 +0330

    add loading

---
+++ Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Peyman on 2/11/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandprimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandprimary = UIColor(named:"brandPrimary")
+}
